5bc22e505653916bab9df4b330f3c4c63a1133bc
Guillaume Ader <guillaumeader@Guillaumes-MacBook-Pro.local>
2019-11-29 13:02:17 +0000

    add doc link

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 // https://risanb.com/posts/bundling-your-javascript-library-with-rollup/
 // https://zellwk.com/blog/publish-to-npm/
-// hot reload : https://lengstorf.com/learn-rollup-css/
+// full lib project: https://github.com/alex996/react-css-spinners
 const path = 'dist/lib';
 const name = 'audioAnalayser';
 export default {
