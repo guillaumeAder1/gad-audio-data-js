f3cc36408f1d0dd8552d9994840016d10adaaa1b
Guillaume Ader <guillaumeader@Guillaumes-MacBook-Pro.local>
2019-11-29 17:47:32 +0000

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,25 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import config from './package.json';
 // https://risanb.com/posts/bundling-your-javascript-library-with-rollup/
 // https://zellwk.com/blog/publish-to-npm/
 // full lib project: https://github.com/alex996/react-css-spinners
 const path = 'dist/lib';
-const name = 'audioAnalayser';
+const name = 'AudioTool';
+const bundle = 'bundle';
+
 export default {
   input: 'src/index.js',
   output: [{
-    file:  `${path}/${name}.cjs.js`,
+    file:  `${path}/${bundle}.cjs.js`,
     format: 'cjs',
   }, {
-    file: `${path}/${name}.esm.js`,
+    file: `${path}/${bundle}.esm.js`,
     format: 'esm'
   }, {
-    file: `${path}/${name}.umd.js`,
+    file: `${path}/${bundle}.umd.js`,
     format: 'umd',
-    name: 'AudioAnalyser'
+    name: name
   }],
   watch: {
     include: 'src/**'
