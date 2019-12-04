fb1e8fdbd901948c36cdb9c76b2271bf418b23f4
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-12-04 21:34:14 +0000

    lastest build

--- dist/lib/bundle.umd.js
+++ dist/lib/bundle.umd.js
@@ -42,13 +42,14 @@
     }
     /**
      * @param {HTML5 Audio Element} player - audio element playing the song
+     * @param {Number} fft - frequency array size, 64 return array.length = 32
      */
 
 
     _createClass(_default, [{
       key: "createAnalyzer",
       value: function createAnalyzer(player) {
-        var fft = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 64;
+        var fft = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 32;
 
         try {
           var context = new (window.AudioContext || window.webkitAudioContext)();
@@ -96,7 +97,64 @@
     return _default;
   }();
 
+  var _default$1 =
+  /*#__PURE__*/
+  function () {
+    function _default(props) {
+      _classCallCheck(this, _default);
+
+      this.canvas = props.canvas;
+      this.ctx = this.canvas.getContext('2d');
+      this.max = 255;
+      this.color = props.color || '#00a0dd';
+      this.name = props.name || '..Bar..';
+      this.setCanvasSize(this.canvas);
+    }
+
+    _createClass(_default, [{
+      key: "setCanvasSize",
+      value: function setCanvasSize(canvas) {
+        this.width = canvas.width;
+        this.height = canvas.height;
+      }
+    }, {
+      key: "calcY",
+      value: function calcY(val) {
+        var perc = val / this.max;
+        return this.height - this.height * perc;
+      }
+    }, {
+      key: "draw",
+      value: function draw(data) {
+        console.log(this.name);
+        var ctx = this.ctx,
+            width = this.width,
+            height = this.height;
+        var step = width / data.length; // clear before redraw
+
+        ctx.clearRect(0, 0, width, height); // draw bg color
+
+        ctx.fillStyle = '#141414';
+        ctx.fillRect(0, 0, width, height); // start drawing bars
+
+        for (var i = 0; i < data.length; i++) {
+          ctx.beginPath();
+          ctx.lineWidth = "2";
+          ctx.strokeStyle = "red";
+          ctx.rect(i * step, this.calcY(data[i]), step, height);
+          ctx.stroke();
+        }
+      }
+    }, {
+      key: "destroy",
+      value: function destroy() {}
+    }]);
+
+    return _default;
+  }();
+
   exports.AudioAnalyser = _default;
+  exports.Histogram = _default$1;
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
