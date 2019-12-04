dd7836f681f244cc10ae3c570219159fd82f6eda
Guillaume Ader <guillaumeader@Guillaumes-MacBook-Pro.local>
2019-12-04 17:30:17 +0000

    feat: start confguring test with jest

---
+++ src/audio/AudioAnalayser.test.js
@@ -0,0 +1,8 @@
+import AudioAnaylser  from "./AudioAnalyser";
+
+describe('AudioAnalyser', () => {
+  it('it should have a contructor', () => {
+    expect(AudioAnaylser).not.toBe(null);
+
+  })
+})
