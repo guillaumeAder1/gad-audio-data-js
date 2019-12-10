68965cfa85bfc3022a4542eba0f7c10d161a3b0b
Guillaume Ader <guillaumeader@Guillaumes-MacBook-Pro.local>
2019-12-10 17:48:11 +0000

    fix mock calculate position

--- src/animations/Histogram.test.js
+++ src/animations/Histogram.test.js
@@ -12,6 +12,16 @@ const canvasMock = {
   width: 300,
   height: 300
 }
+const mockCalculate = jest.fn(() => ({
+  x: 2,
+  y: 2,
+  height: 2,
+  width: 3
+}))
+Histogram.prototype.calculatePosition = mockCalculate
+beforeEach(() => {
+  mockCalculate.mockClear()
+})
 describe('Histogram', () => {
   it('should create instance as expected', () => {
     const histo = new Histogram(canvasMock)
@@ -25,29 +35,21 @@ describe('Histogram', () => {
     expect(histo.height).toEqual(300)
   })
   describe('should calculate position', () => {
-    beforeEach(() => {
-      // jest.mockReset()
-    })
     it('shoudl call calculate position for every item recevied as [data]', () => {
-      Histogram.prototype.calculatePosition = jest.fn(() => ({
-        x: 2,
-        y: 2,
-        width: 2,
-        height: 2
-      }))
       const histo = new Histogram(canvasMock)
       histo.draw([150, 0, 300])
       expect(histo.calculatePosition).toHaveBeenCalledTimes(3)
-      Histogram.prototype.calculatePosition.mockClear()
+      mockCalculate.mockClear()
       histo.draw([1, 2, 3, 4, 5, 6])
       expect(histo.calculatePosition).toHaveBeenCalledTimes(6)
-      // Histogram.prototype.calculatePosition.mockReset()
     })
     it('should calculate position for botton algin', () => {
-      // const histo = new Histogram(canvasMock, 'bottom')
-      // const data = [300, 150, 0]
-      // histo.draw(data)
-      // expect(histo.calculatePosition).toHaveBeenCalledWith(0, 0, 300)
+      const histo = new Histogram(canvasMock, 'bottom')
+      const data = [300, 150, 0]
+      histo.draw(data)
+      expect(histo.calculatePosition).toHaveBeenCalledWith(100, 0, 300)
+      expect(histo.calculatePosition).toHaveBeenCalledWith(100, 1, 150)
+      expect(histo.calculatePosition).toHaveBeenCalledWith(100, 2, 0)
     })
   })
 })
